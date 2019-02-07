17ce5a9356839c97c3de3b7361bca12c8074ba3d
Lorenzo Gabarda <lorenzo.gabarda@shoresuite.com>
2019-02-07 10:15:23 +0800

    Added basic math operators

--- assets/js/components/Calculator.js
+++ assets/js/components/Calculator.js
@@ -6,52 +6,92 @@ class Calculator extends Component {
         super(props)
 
         this.state = {
-            valueTotal: ''
+            valueFinal: '',
+            valueFirstNumber: '',
+            valueComp: ''
         }
     }
 
     clickHandler = (event) => {
-        this.setState({ valueTotal: this.state.valueTotal + event.target.value })
+        this.setState({
+            valueFinal: this.state.valueFinal + event.target.value
+
+        })
     }
 
-    clickDivide = (event) => {
-        if (event.target.value == "/") {
-            console.log('it is divide')
-        }
+    clickCompute = (event) => {
+
+        this.setState({
+            valueFirstNumber: this.state.valueFinal,
+            valueFinal: '',
+            valueComp: event.target.value
+        })
+
     }
+
+    clickTotal = (event) => {
+        if (this.state.valueComp == '/') {
+            this.setState({
+                valueFinal: parseInt(this.state.valueFirstNumber) / parseInt(this.state.valueFinal)
+            })
+        }
+        if (this.state.valueComp == '*') {
+            this.setState({
+                valueFinal: parseInt(this.state.valueFirstNumber) * parseInt(this.state.valueFinal)
+            })
+        }
+        if (this.state.valueComp == '-') {
+            this.setState({
+                valueFinal: parseInt(this.state.valueFirstNumber) - parseInt(this.state.valueFinal)
+            })
+        }
+        if (this.state.valueComp == '+') {
+            this.setState({
+                valueFinal: parseInt(this.state.valueFirstNumber) + parseInt(this.state.valueFinal)
+            })
+        }
+
+    }
+
+
+    clickClear = (evenet => {
+        this.setState({ valueFinal: '' })
+    })
     render() {
         return (
             <div>
                 <table>
                     <tbody>
                         <td>
-                            <input type="text" value={this.state.valueTotal} />
+                            <input type="text" value={this.state.valueFinal} />
                         </td>
 
                         <tr>
                             <button value='1' onClick={this.clickHandler}>1</button>
                             <button value="2" onClick={this.clickHandler}>2</button>
                             <button value="3" onClick={this.clickHandler}>3</button>
-                            <button value="/" onClick={this.clickDivide}>/</button>
+                            <button value="/" onClick={this.clickCompute}>/</button>
                         </tr>
 
                         <tr>
-                            <button value="4">4</button>
-                            <button value="5">5</button>
-                            <button value="6">6</button>
-                            <button value="*">*</button>
+                            <button value="4" onClick={this.clickHandler}>4</button>
+                            <button value="5" onClick={this.clickHandler}>5</button>
+                            <button value="6" onClick={this.clickHandler}>6</button>
+                            <button value="*" onClick={this.clickCompute}>*</button>
                         </tr>
 
                         <tr>
-                            <button value="7">7</button>
-                            <button value="8">8</button>
-                            <button value="9">9</button>
-                            <button value="-">-</button>
+                            <button value="7" onClick={this.clickHandler}>7</button>
+                            <button value="8" onClick={this.clickHandler}>8</button>
+                            <button value="9" onClick={this.clickHandler}>9</button>
+                            <button value="-" onClick={this.clickCompute}>-</button>
                         </tr>
                         <tr>
-                            <button value="0">0</button>
-                            <button value="+">+</button>
-                            <button>=</button>
+                            <button value="C" onClick={this.clickClear}>C</button>
+                            <button value="0" onClick={this.clickHandler}>0</button>
+                            <button value="=" onClick={this.clickTotal}>=</button>
+                            <button value="+" onClick={this.clickCompute}>+</button>
+
                         </tr>
 
                     </tbody >
